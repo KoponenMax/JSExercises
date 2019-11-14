cdd3f5c8fea1d1d66e0835ddb97345aafb0d5203
KoponenMax <victor12.com@mail.ru>
2019-11-14 22:06:08 +0300

    The new beginning

--- script.js
+++ script.js
@@ -1,17 +1,19 @@
-let money = prompt("Ваш бюджет на месяц?");
+let money = +prompt("Ваш бюджет на месяц?");
 let time = prompt("Введите дату в формате YYYY-MM-DD");
 
 let appData = {
     budget: money,
-    timeData: time,
     expenses: {},
     optionalExpenses: {},
     income: [],
+    timeData: time,
     savings: false,  
 };
+let a1 = prompt("Введите обязательную статью доходов в этом месяце", ''),
+    a2 = prompt("Во сколько обойдется?"),
+    a3 = prompt("Введите обязательную статью доходов в этом месяце", ''),
+    a4 = prompt("Во сколько обойдется?");
 
-var whatExpenses = prompt("Введите обязательную статью расходов в этом месяце");
-var howManyExpenses = prompt("Во сколько обойдется?");
 alert(appData.budget/30);
 
 
